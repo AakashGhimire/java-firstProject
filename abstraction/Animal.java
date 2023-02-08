b7e3eaa096d65b4acef849562e5a0ef20c0015f3
Aakash Ghimire <aakash.ghimire21@gmail.com>
2023-02-07 21:04:53 -0600

    added walk method in animal class

--- abstraction/Animal.java
+++ abstraction/Animal.java
@@ -6,6 +6,10 @@ public abstract class Animal {
 	public void run() {
 		System.out.println("animals cannot run");
 	}
+
+	public void walk(){
+		System.out.println("animals can walk");
+	}
 	
 	public abstract void jump();
 }
